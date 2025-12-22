bef2a47b9268ee8bfe668fa86fd6ce0d34f2e701
lebaohungk05 <hungk1210@gmail.com>
2025-12-22 20:56:28 +0700

    Initial commit: LMS App with Auth, Database, and Course Setup UI

--- app/dashboard/_components/sidebar-item.tsx
+++ app/dashboard/_components/sidebar-item.tsx
@@ -1,8 +1,9 @@
-'use client';
+"use client";
 
-import { LucideIcon } from 'lucide-react';
-import { usePathname, useRouter } from 'next/navigation';
-import { cn } from '@/lib/utils';
+import { LucideIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
 
 interface SidebarItemProps {
   icon: LucideIcon;
@@ -14,10 +15,10 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
   const pathname = usePathname();
   const router = useRouter();
 
-  const isActive = 
-    (pathname === href) || 
-    (href === '/' && pathname === '/') || 
-    (href !== '/' && pathname?.startsWith(`${href}/`));
+  const isActive =
+    (pathname === "/" && href === "/") ||
+    pathname === href ||
+    pathname?.startsWith(`${href}/`);
 
   const onClick = () => {
     router.push(href);
@@ -29,7 +30,8 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
       type="button"
       className={cn(
         "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
-        isActive && "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+        isActive &&
+          "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
       )}
     >
       <div className="flex items-center gap-x-2 py-4">
